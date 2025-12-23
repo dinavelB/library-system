6dbb391874b9628e3c8a30af6aa18cc441d2bcd1
Dinavel Binongo <binongo.dinavel.escodero@gmail.com>
2025-12-23 21:34:46 +0800

    fixed the database connection, created a user

--- react/myDatabase.js
+++ react/myDatabase.js
@@ -2,8 +2,8 @@ import mysql from "mysql2";
 
 const pool = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "dB020606",
+  user: "appuser",
+  password: "app1234",
   database: "my_first-Db",
   waitForConnections: true,
   connectionLimit: 5,
