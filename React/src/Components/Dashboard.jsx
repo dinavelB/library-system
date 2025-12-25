8a08de9a610bbca92bfd97da0b0bfed38260953d
Dinavel Binongo <binongo.dinavel.escodero@gmail.com>
2025-12-25 21:43:33 +0800

    fixing UI for dashboard library

--- React/src/Components/Dashboard.jsx
+++ React/src/Components/Dashboard.jsx
@@ -1,9 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import "../index.css";
 
 function NavigationBar() {
   return (
-    <nav>
+    <nav className="navigation-container">
       <div className="me-btn">
         <FontAwesomeIcon icon={faUser} />
         <label htmlFor="">Me</label>
@@ -13,10 +14,48 @@ function NavigationBar() {
   );
 }
 
+function Category() {
+  return (
+    <nav className="category-container">
+      <ul>
+        <li>Fiction</li>
+        <li>Romance</li>
+        <li>Horror</li>
+        <li>Educational</li>
+        <li>History</li>
+      </ul>
+    </nav>
+  );
+}
+
+function Books() {
+  return (
+    <section className="book-container">
+      <div>
+        <h1>Book</h1>
+      </div>
+      <div>
+        <h1>Book</h1>
+      </div>
+      <div>
+        <h1>Book</h1>
+      </div>
+      <div>
+        <h1>Book</h1>
+      </div>
+      <div>
+        <h1>Book</h1>
+      </div>
+    </section>
+  );
+}
+
 export default function Dashboard() {
   return (
     <>
       <NavigationBar />
+      <Category />
+      <Books />
     </>
   );
 }
