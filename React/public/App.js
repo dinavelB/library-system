ab46dedaf32f38e72e973910f82536696b12adf0
Dinavel Binongo <binongo.dinavel.escodero@gmail.com>
2025-12-25 12:50:53 +0800

    git rm --cached folders i want to ignore

--- React/public/App.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-import Navigation from "./Components/Navigation";
-import { jsx as _jsx } from "react/jsx-runtime";
-function App() {
-  return /*#__PURE__*/_jsx(Navigation, {});
-}
-export default App;
--- React/public/main.js
+++
@@ -1,7 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { jsx as _jsx } from "react/jsx-runtime";
-createRoot(document.getElementById("root")).render(/*#__PURE__*/_jsx(StrictMode, {
-  children: /*#__PURE__*/_jsx(App, {})
-}));
