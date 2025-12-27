f0b6e1f2dbac6c29bca868a4197e55ace69a5ca5
Dinavel Binongo <binongo.dinavel.escodero@gmail.com>
2025-12-27 17:19:52 +0800

    adding loading effect

--- React/src/Components/Dashboard.jsx
+++ React/src/Components/Dashboard.jsx
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import "../index.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function NavigationBar() {
+function NavigationBar({ animation, setAnimation }) {
   return (
-    <nav className="navigation-container">
+    <nav className={`navigation-container${animation ? "active" : ""}`}>
       <div className="me-btn">
         <FontAwesomeIcon icon={faUser} />
         <label htmlFor="">Me</label>
@@ -15,15 +15,21 @@ function NavigationBar() {
   );
 }
 
-function Category() {
+function Category({ bgColor, setBgColor }) {
+  const categories = ["Fiction", "Romance", "Horror", "Educational", "History"];
+
   return (
     <nav className="category-container">
       <ul className="categories">
-        <li>Fiction</li>
-        <li>Romance</li>
-        <li>Horror</li>
-        <li>Educational</li>
-        <li>History</li>
+        {categories.map((category) => (
+          <li
+            key={category}
+            className={bgColor === category ? "active" : ""}
+            onClick={() => setBgColor(category)}
+          >
+            {category}
+          </li>
+        ))}
       </ul>
     </nav>
   );
@@ -52,12 +58,31 @@ function Books() {
 }
 
 export default function Dashboard() {
-  const [categoryColor, setCategoryColor] = useState(null);
+  const [nav, setNav] = useState(false);
+  const [cat, setCat] = useState(false);
+  const [books, setBooks] = useState(false);
+
+  const [animation, setAnimation] = useState(null);
+
+  useEffect(() => {
+    const loadTimer = [];
+
+    loadTimer.push(setTimeout(() => setNav(true), 500));
+    loadTimer.push(setTimeout(() => setCat(true), 200));
+    loadTimer.push(
+      setTimeout(() => setBooks(true)),
+      500
+    );
+  }, []);
   return (
     <>
-      <NavigationBar />
-      <Category />
-      <Books />
+      {nav && (
+        <NavigationBar animation={animation} setAnimation={setAnimation} />
+      )}
+      {cat && (
+        <Category bgColor={categoryColor} setBgColor={setCategoryColor} />
+      )}
+      {books && <Books />}
     </>
   );
 }
