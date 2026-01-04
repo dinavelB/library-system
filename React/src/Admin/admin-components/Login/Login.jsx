c7662c7d50dd4773f6edb8c325a692a89a7b2cb3
Dinavel Binongo <binongo.dinavel.escodero@gmail.com>
2026-01-04 16:07:52 +0800

    untracked some files

--- React/src/Admin/admin-components/Login/Login.jsx
+++ React/src/Admin/admin-components/Login/Login.jsx
@@ -1,3 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
-  return <h1>I am a login page</h1>;
+  const nav = useNavigate();
+
+  const loginbtn = () => {
+    nav("/dashboard");
+  };
+  return (
+    <div>
+      <h1>I am a login page</h1>
+      <button onClick={loginbtn}>Login</button>
+    </div>
+  );
 }
--- React/src/Admin/admin-components/Navigation.jsx
+++ React/src/Admin/admin-components/Navigation.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login/Login";
+import Dashboard from "./Dashboard/Dashboard";
 
 export default function Navigation() {
   return (
     <BrowserRouter basename="/admin">
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
