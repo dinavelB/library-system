1965776543affe8960baaf67c305de42f96ee47c
Dinavel Binongo <binongo.dinavel.escodero@gmail.com>
2026-01-06 12:54:54 +0800

    modular, and add tokens

--- React/src/Admin/admin-components/Dashboard/Dashboard.jsx
+++ React/src/Admin/admin-components/Dashboard/Dashboard.jsx
@@ -1,3 +1,24 @@
+import { useEffect } from "react";
+
 export default function Dashboard() {
-  return <h1>Im a dashboard</h1>;
+  const adminToken = localStorage.getItem("adminRoute"); //take the string only or key
+
+  const sampleVerify = async () => {
+    const sendData = await fetch("/dashboard", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${adminToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await sendData.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    sampleVerify();
+  }, []);
+
+  return <h1>helo</h1>;
 }
--- React/src/Admin/admin-components/Login/Login.jsx
+++ React/src/Admin/admin-components/Login/Login.jsx
@@ -1,10 +1,37 @@
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
+import { useState } from "react";
+
 export default function Login() {
   const nav = useNavigate();
 
-  const loginbtn = () => {
-    nav("/dashboard");
+  const [adminData, setAdminData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const getData = (e) => {
+    const { name, value } = e.target;
+    setAdminData((prev) => ({
+      ...prev, //prevents rewriting the declared state, states only receives one argument
+      [name]: value,
+    }));
+  };
+
+  const loginAdmin = async () => {
+    const dBResponse = await fetch("/admin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(adminData),
+    });
+
+    const printRes = await dBResponse.json();
+
+    if (dBResponse.ok) {
+      console.log("admin logged in successfully");
+      localStorage.setItem("adminToken", printRes.token);
+      nav("/dashboard");
+    }
   };
   return (
     <section>
@@ -19,10 +46,20 @@ export default function Login() {
             <p>Enter credentials to login as admin</p>
           </span>
           <label htmlFor="">Username</label>
-          <input type="text" placeholder="Enter username" />
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter username"
+            onChange={getData}
+          />
           <label htmlFor="">Password</label>
-          <input type="password" placeholder="Enter your password" />
-          <button>Sign-in</button>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            onChange={getData}
+          />
+          <button onClick={loginAdmin}>Sign-in</button>
         </div>
       </div>
     </section>
