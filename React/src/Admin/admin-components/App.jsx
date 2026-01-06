7a0a9787e02f3c8c9964fad6e4386ce7d8303505
Dinavel Binongo <binongo.dinavel.escodero@gmail.com>
2026-01-06 20:43:16 +0800

    dashboard

--- React/src/Admin/admin-components/App.jsx
+++ React/src/Admin/admin-components/App.jsx
@@ -1,5 +1,6 @@
 import Navigation from "./Navigation";
+import Dashboard from "./Dashboard/Dashboard";
 
 export default function App() {
-  return <Navigation />;
+  return <Dashboard />;
 }
--- React/src/Admin/admin-components/Dashboard/Dashboard.jsx
+++ React/src/Admin/admin-components/Dashboard/Dashboard.jsx
@@ -1,6 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import "../../../CSS/Dashboard.css";
+import dashboard from "../../../Icons/dashboard-icon.png";
+import book from "../../../Icons/book-icon.png";
+import users from "../../../Icons/users-icon.png";
+import details from "../../../Icons/details-icon.png";
+import search from "../../../Icons/search-icon.png";
 
 export default function Dashboard() {
+  const [active, setActive] = useState(null);
   const token = localStorage.getItem("adminToken"); //take the string only or key
 
   const sampleVerify = async () => {
@@ -20,5 +27,41 @@ export default function Dashboard() {
     sampleVerify();
   }, []);
 
-  return <h1>helo</h1>;
+  const options = [
+    { navigate: "Dashboard", icon: dashboard },
+    { navigate: "Books", icon: book },
+    { navigate: "Users", icon: users },
+    { navigate: "Book Details", icon: details },
+  ];
+
+  return (
+    <section className="dashboard-container">
+      <div className="dashboard-holder">
+        <nav className="navigation-container">
+          <span className="logo">
+            Ed<span>Library</span>
+          </span>
+          <ul>
+            {options.map((option) => (
+              <li
+                key={option.navigate}
+                onClick={() => setActive(option.navigate)}
+                className={`nav ${active === option.navigate ? "active" : ""}`}
+              >
+                <img src={option.icon} alt="" />
+                {option.navigate}
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="main-board-search">
+          <div className="input-holder">
+            <img src={search} alt="" />
+            <input type="text" placeholder="Search" im />
+          </div>
+          <div className="main-board"></div>
+        </div>
+      </div>
+    </section>
+  );
 }
